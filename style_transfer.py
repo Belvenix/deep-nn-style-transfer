85cef52c523f642b0c62455ac06ee4e2d865bb9e
walusus <marcinw.99.mw@gmail.com>
2020-02-25 18:46:42 +0100

    change function declaration

--- style_transfer.py
+++ style_transfer.py
@@ -180,7 +180,6 @@ def rebuild_model(nn_model, content_image, style_image,
             style_layers_list.append(style_layer)
             last_significant_layer = i + 1
 
-
     # Add this point our new model is the same as input model but with
     # StyleLayers and ContentLayers inserted after required layers
     # we don't need any layers after the last style or content layer
@@ -200,9 +199,8 @@ def get_optimizer(input_img):
     return optimizer
 
 
-
 # 6. Write training function
-def style_transfer():
+def style_transfer(nn_model, style_layers, content_layers):
     """Runs the style transfer on input image"""
     # Add code here
     pass
@@ -243,7 +241,9 @@ if __name__ == '__main__':
     show_tensor(content_tensor_image)
     show_tensor(style_tensor_image)
 
-    new_model,x,y = rebuild_model(model,content_tensor_image.unsqueeze(0),style_tensor_image.unsqueeze(0),mean,std,content_layers_req,style_layers_req)
+    new_model, style_layers, content_layers = rebuild_model(model, content_tensor_image.unsqueeze(0),
+                                                            style_tensor_image.unsqueeze(0), mean, std,
+                                                            content_layers_req, style_layers_req)
     pprint.pprint(new_model)
     # Run style transfer
 
@@ -252,6 +252,3 @@ if __name__ == '__main__':
     # Show results
 
     # Add code here
-
-    # Testing the gitHub ~ Jakub
-    # Testing the gitHub ~ Jakub
