b159ae760dd848213a365615473ca7d9f98cebb2
unknown <kuba.belter@gmail.com>
2020-02-27 18:07:11 +0100

    Started working on server.py file. Mostly fooling around without much productive code

--- server/server.py
+++ server/server.py
@@ -1,6 +1,12 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/var/www/uploads/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 # TASKS:
 # 1. Write function that loads the model and prepares it on server launch
@@ -10,6 +16,35 @@ app = Flask(__name__)
 # 3.1 Write a simple front that shows progress or gives the user information that the image is being generated
 # 4. Present the result for user in simple front, maybe add download button so user can download the new image
 
-@app.route('/')
-def hello():
-    return render_template('hello.html');
+#@app.route('/')
+#def hello():
+#    return render_template('hello.html')
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        f = request.files['the file']
+        if f.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if f and allowed_file(f.filename):
+            filename = secure_filename(f.filename)
+            f.save(app.config['UPLOAD_FOLDER'] + secure_filename(f.filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
