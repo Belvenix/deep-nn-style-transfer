78df7c6f59c662f138ba9c2d4919f77476fc38fe
unknown <kuba.belter@gmail.com>
2020-02-27 22:50:30 +0100

    Heavy work has been done: - .gitignore eddited to make sure that session is not being updated all the time (I used in in our app) - server.py has been eddited and I created a simple form for uploading the files and it works. - added more files for html to be used in our app like 	- main.css in order to control the style of our application 	- index.html to make there our entire app, possibly to extend to different html files 	- page_template.html to make use of Jinja syntax if we were to use more html files

--- server/server.py
+++ server/server.py
@@ -1,12 +1,16 @@
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request, redirect, url_for, render_template, session
 from werkzeug.utils import secure_filename
+from flask_session import Session
 
+app = Flask(__name__)
+sess = Session()
+
+SESSION_TYPE = 'filesystem'
 UPLOAD_FOLDER = '/var/www/uploads/'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+app.config['SESSION_TYPE'] = SESSION_TYPE
 
 # TASKS:
 # 1. Write function that loads the model and prepares it on server launch
@@ -16,9 +20,15 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # 3.1 Write a simple front that shows progress or gives the user information that the image is being generated
 # 4. Present the result for user in simple front, maybe add download button so user can download the new image
 
-#@app.route('/')
-#def hello():
-#    return render_template('hello.html')
+@app.route('/xyz', methods=['POST','GET'])
+def index():
+    if request.method == 'POST':
+        user_image = request.form['myfile']
+        print(type(user_image))
+        #return 'File was submitted. Please wait patiently.'
+    else:
+        pass
+        #return render_template('index.html')
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -27,24 +37,24 @@ def allowed_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        if 'file' not in request.files:
+        if 'myfile' not in request.files:
             flash('No file part')
             return redirect(request.url)
-        f = request.files['the file']
+        f = request.files['myfile']
         if f.filename == '':
             flash('No selected file')
             return redirect(request.url)
         if f and allowed_file(f.filename):
             filename = secure_filename(f.filename)
+            print(filename)
             f.save(app.config['UPLOAD_FOLDER'] + secure_filename(f.filename))
             return redirect(url_for('uploaded_file',
                                     filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    sess.init_app(app)
+    app.run(debug=True)
