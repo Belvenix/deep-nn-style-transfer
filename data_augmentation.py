30167c4d36a60ac35b0e7325d61209545e2cf2e9
jbelter <kuba.belter@gmail.com>
2020-11-21 20:10:09 +0100

    Feature - start working on data augmentation for our dataset

---
+++ data_augmentation.py
@@ -0,0 +1,38 @@
+import os
+
+from keras.preprocessing.image import ImageDataGenerator
+
+from utils import DNNConfigurer
+
+datagen = ImageDataGenerator(
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+
+def rename_files():
+    print("Renaming files")
+    directory_content_t = DNNConfigurer["data_files"]["CONTENT_TRANSFORMED_ROOT"]
+    directory_style_t = DNNConfigurer["data_files"]["STYLE_TRANSFORMED_ROOT"]
+    i = 1
+    for filename in os.listdir(directory_content_t):
+        filename = os.path.join(directory_content_t, filename)
+        new_file_name = directory_content_t + "content_" + str(i) + ".jpg"
+        os.rename(filename, new_file_name)
+        i += 1
+
+    i = 1
+    for filename in os.listdir(directory_style_t):
+        filename = os.path.join(directory_style_t, filename)
+        new_file_name = directory_style_t + "style_" + str(i) + ".jpg"
+        os.rename(filename, new_file_name)
+        i += 1
+
+
+if __name__ == '__main__':
+    rename_files()
